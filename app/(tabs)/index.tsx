8932ac5273d006bc7458f76f08050f244d39f964
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2026-01-28 11:17:42 +0100

    feat: tailwind css

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,6 +7,7 @@ import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
 
+// noinspection JSUnusedGlobalSymbols
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
